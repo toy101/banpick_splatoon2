3b148729e3e9a1d1c0d23f04940973e27481eee2
toy101 <toy101dev@gmail.com>
2020-01-16 20:35:50 +0900

    completed

--- manager.py
+++ manager.py
@@ -20,18 +20,36 @@ class BanPickManager():
         self.member = np.loadtxt("member.csv", delimiter=',', dtype=str,
                                  encoding='shift_jis')
         self.stages = all_stage
-        self.alpha = []
-        self.beta = []
 
-        self.game_flag = False
-        self.banpick_flag = False
+        # self.alpha = []
+        # self.beta = []
+
+        # self.game_flag = False
+        # self.banpick_flag = False
 
         self.ab_list = ["α","β","α","β",
                         "α","β","β","α",
                         "β","α","β","α",
                         "β","α","α","β"]
         self.ban_pick = ["バン", "ピック"]
+        # self.count = 0
+        # self.ban_list = {"α":[], "β":[]}
+        # self.pick_list = {"α": [], "β": []}
+
+        self._reset()
+
+    def _reset(self):
+        self.alpha = []
+        self.beta = []
+
+        self.game_flag = False
+        self.banpick_flag = False
+
         self.count = 0
+        self.ban_list = {"α": [], "β": []}
+        self.pick_list = {"α": [], "β": []}
+
+        self.last_msg = ""
 
     def controller(self, content):
 
@@ -48,25 +66,60 @@ class BanPickManager():
             msg += self.choose_and_lost_stage()
             msg += "\n\n"
 
+            self.last_msg += msg
+
             self.banpick_flag = True
             msg += "バンピックを開始します\n"
             msg += "\n"
 
-        if self.count%4 == 0:
+        if not self.banpick_flag:
+            return "不正な入力です"
+
+        if self.count > 0:
+            self.register(content, self.count-1)
+
+        if self.count%4 == 0 and self.count < 16:
+            if self.count > 0:
+                msg += self.output_weapons_name()
+
             msg += self.make_massage(self.count)
             msg += "\n\n"
 
+        if self.count >= 16:
+            msg += "全てのバンピックが終わりました。ゲームスタートです!\n\n"
+            msg += self.last_msg + self.output_weapons_name()
+            self._reset()
 
-        msg += "{}チーム、{}するブキを選んでください".format(
-                self.ab_list[self.count], self.ban_pick[self.count//4]
-            )
+            return msg
+        else:
+            msg += "{}チーム、{}するブキを選んでください".format(
+                    self.ab_list[self.count], self.ban_pick[self.count//4%2]
+                )
 
         self.count += 1
 
         return msg
 
-    def banpick(self, weapon=None):
-        pass
+    def output_weapons_name(self):
+
+        msg = ""
+
+        msg += "バンしたブキ\n"
+        msg += "αチーム:{}\n".format(list_to_str(self.ban_list["α"]))
+        msg += "βチーム:{}\n".format(list_to_str(self.ban_list["β"]))
+        msg += "ピックしたブキ\n"
+        msg += "αチーム:{}\n".format(list_to_str(self.pick_list["α"]))
+        msg += "βチーム:{}\n".format(list_to_str(self.pick_list["β"]))
+
+        msg += "\n"
+
+        return msg
+
+    def register(self, weapon, num):
+        if self.ban_pick[num//4%2] == "バン":
+            self.ban_list[self.ab_list[num]].append(weapon)
+        else:
+            self.pick_list[self.ab_list[num]].append(weapon)
 
     def make_massage(self, num):
         # バンピックの順番を指示するメッセージを生成
@@ -77,7 +130,7 @@ class BanPickManager():
             if i != 3:
                 msg += "→"
 
-        return "{}の順で{}していきます".format(msg, self.ban_pick[num//4])
+        return "{}の順で{}していきます".format(msg, self.ban_pick[num//4%2])
 
     def choose_up_teams(self):
         num_list = np.asarray([i for i in range(8)])
